bab641f35bd3447cd9968acf5338293380bd27d4
purgatoryalt <102757720+purgatoryalt@users.noreply.github.com>
2022-05-09 15:29:34 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+let notas = [8, 7, 10]
+console.log (notas)
+
+console.log([0, 1, 2])
+console.log(notas.length)
+
+let media = (notas [0]+ notas[1] + notas[2])/notas.length
+console.log(media)
---
+++ arraySeparar.js
@@ -0,0 +1,6 @@
+const chamada = ['Rai', 'Ruan', 'Xablau', 'MaridasDores', 'Felipe', 'Danilo', 'Daniel', 'Erico']
+console.log(chamada)
+
+let grupo1 = chamada.slice (0, chamada.length/2)
+let grupo2 = chamada.slice (chamada.length/2)
+console.log(grupo1, grupo2)
